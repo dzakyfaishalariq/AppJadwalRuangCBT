ccc3b3f88d132075d9314d93e6f6981739090486
Dzaky_Faishalariq003 <dzakyfaishalariq@gmail.com>
2022-09-09 19:48:35 +0700

    update tampilan login

--- app/Http/Controllers/WebController.php
+++ app/Http/Controllers/WebController.php
@@ -6,5 +6,10 @@ use Illuminate\Http\Request;
 
 class WebController extends Controller
 {
-    //
+    // Area Login
+    public function index()
+    {
+        $title = "Login";
+        return view('login', ['title' => $title]);
+    }
 }
---
+++ resources/views/login.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en" data-theme="night">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
+    />
+    <title>PLK|{{ $title }}</title>
+</head>
+<body>
+    <div class=" container-fluid">
+        <div class="row">
+            <div class=" col-md-4 offset-md-4">
+                <div class="card m-5 shadow-lg bg-body animate__animated animate__bounceIn">
+                    <div class=" card-header text-center bg-info">
+                        <h4>Penjadwalan Laboratorium Komputer</h4>
+                    </div>
+                    <div class="card-body">
+                        <form action="" method="post">
+                            <div class="form-group mb-3">
+                                <input type="text" class="form-control text-center" placeholder="Username" name="user" required>
+                            </div>
+                            <div class="form-group mb-3">
+                                <input type="password" class="form-control text-center" placeholder="Password" name="pass" required>
+                            </div>
+                            <div class="d-grid gap-2 col-6 mx-auto">
+                                <button type="submit" class="btn btn-success mb-3">Login</button>
+                                <a href="#" class=" text-center">Register</a>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+</body>
+</html>
